b42ee903a1a6543af8b78b9ff11c372e76895119
Clayton Treder <cjtreder@gmail.com>
2022-01-21 09:53:05 -0500

    first real comit

--- src/content/pages/home.md
+++ src/content/pages/home.md
@@ -1,47 +1,50 @@
 ---
-site-logo: images/logo.svg
-home-bg: images/chip.jpg
-home-call-to-action:
-  hcta-lines:
-    - hcta-line: Detroit-based software developers
-    - hcta-line: Ready to take on your next project
-  hcta-buttons:
-    - hcta-button:
-        hcta-button-text: I need a website
-        hcta-button-link: /services#site
-    - hcta-button:
-        hcta-button-text: I need an app
-        hcta-button-link: /services#app
-    - hcta-button:
-        hcta-button-text: I need software
-        hcta-button-link: /services#software
-home-news-letter:
-  hnl-title: News Letter
-  hnl-placeholder: Email *
-  hnl-text: images/chip2.jpg
-home-actions:
-  - ha-action:
-      ha-action-text: We use the latest techniques and modern designs to build or
+site_logo: images/logo.svg
+home_bg: images/chip.jpg
+home_call_to_action:
+  hcta_lines:
+    - hcta_line: Detroit-based software developers
+    - hcta_line: Ready to take on your next project
+  hcta_buttons:
+    - hcta_button:
+        hcta_button_text: I need a website
+        hcta_button_link: /services#site
+    - hcta_button:
+        hcta_button_text: I need an app
+        hcta_button_link: /services#app
+    - hcta_button:
+        hcta_button_text: I need software
+        hcta_button_link: /services#software
+home_news_letter:
+  hnl_title: News Letter
+  hnl_placeholder: Email *
+  hnl_text: images/chip2.jpg
+home_actions:
+  - ha_action:
+      ha_action_text:
+        We use the latest techniques and modern designs to build or
         improve your website. Ready for all devices. Mobile friendly layouts
         that convert views to sales quickly.
-      ha-action-image: images/website.webp
-  - ha-action:
-      ha-action-text: Today's ecosystem demands apps to give you a competitive edge.
+      ha_action_image: images/website.webp
+  - ha_action:
+      ha_action_text:
+        Today's ecosystem demands apps to give you a competitive edge.
         Let us help you build an app that exceeds expectations and does the
         amazing things you expect from a professional app.
-      ha-action-image: images/phone.webp
-  - ha-action:
-      ha-action-text: Our developers can create custom software solutions allowing
+      ha_action_image: images/phone.webp
+  - ha_action:
+      ha_action_text:
+        Our developers can create custom software solutions allowing
         your IT infrastructure to save you money. Custom software that will give
         you enterprise control over your business operations.
-      ha-action-image: images/cloud.webp
-home-links:
-  - hl-link:
-      hl-link-text: Schedule a free consultation now
-      hl-link-nav: /contact
-      hl-link-image: images/coffee.png
-  - hl-link:
-      hl-link-nav: /clients
-      hl-link-text: View our portfolio
-      hl-link-image: images/portfolio.png
+      ha_action_image: images/cloud.webp
+home_links:
+  - hl_link:
+      hl_link_text: Schedule a free consultation now
+      hl_link_nav: /contact
+      hl_link_image: images/coffee.png
+  - hl_link:
+      hl_link_nav: /clients
+      hl_link_text: View our portfolio
+      hl_link_image: images/portfolio.png
 ---
--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,46 +1,106 @@
-import Information from "components/Information/Information";
-import NewsLetter from "components/NewsLetter/NewsLetter";
-import Picture from "components/Picture/Picture";
-import useSessionStorage from "hooks/useSessioStorage";
-import { useEffect } from "react";
-import Banner from "../../components/Banner/Banner";
+import { useState } from "react";
 import "./Home.css";
+import { attributes } from "../../content/pages/home.md";
+import axios from "axios";
 
 function Home() {
-  const [text, setText] = useSessionStorage("homeText" + new Date().getHours());
-  useEffect(() => {
-    text ??
-      fetch("/api/text/Home")
+  console.log(attributes);
+  const [email, setEmail] = useState();
+  const [regexFail, setRegexFail] = useState();
+  const [resStatus, setResStatus] = useState();
+
+  const text = attributes;
+
+  const handleClick = () => {
+    if (
+      /^[A-Za-z0-9]{1,}@{1}[A-Za-z0-9]{2,}\.{1}[A-Za-z0-9]{2,5}$/gm.test(
+        email
+      ) === true
+    ) {
+      axios
+        .post(
+          "/api/newsletter",
+          {
+            email: email,
+            subscribed: true,
+            addedOn: new Date(),
+          },
+          { headers: { "Content-Type": "application/json" } }
+        )
         .then((res) => {
-          return res.json();
+          if (res.status === 200) {
+            setResStatus("Success!");
+          } else {
+            setResStatus(
+              "An issue occured, please try again later. You may already be subscribed."
+            );
+          }
         })
-        .then((data) => {
-          setText(() => data.content);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-  });
+        .catch(() =>
+          setResStatus(
+            "An issue occured, please try again later. You may already be subscribed."
+          )
+        );
+    } else {
+      setRegexFail(true);
+      setResStatus(null);
+    }
+  };
+
   return text ? (
     <div>
-      <Picture
-        s3ImgKey={text.pic.s3ImgKey}
-        alt="micro-chip"
-        className="bg-img"
-      />
-      <Banner lines={text.banner.lines} buttons={text.banner.buttons} />
-
-      <div>
-        {text.showNewsLetter ? <NewsLetter /> : null}
-        <Information info={text.information} />
+      <img src={`./${text.home_bg}`} alt="micro-chip" className="bg-img" />
+      <div className="banner fade-in">
+        <title>
+          {text.home_call_to_action.hcta_lines.map((line) => {
+            return <h3>{line.hcta_line}</h3>;
+          })}
+        </title>
+        <ribbon>
+          {text.home_call_to_action.hcta_buttons.map((button) => {
+            return (
+              <a href={button.hcta_button.hcta_button_link}>
+                <button>{button.hcta_button.hcta_button_text}</button>
+              </a>
+            );
+          })}
+        </ribbon>
+      </div>
+      <div className="newsletter">
+        <span>Sign up for our news letter:</span>
+        <div>
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setRegexFail(false);
+            }}
+            placeholder="Email Address*"
+            type="text"
+          />
+          <button onClick={handleClick}>Submit</button>
+        </div>
+        {regexFail ? <div>Please provide a valid email</div> : null}
+        {resStatus ? <div>{resStatus}</div> : null}
+      </div>
+      <div className="information">
+        {text.home_actions.map((action) => {
+          return (
+            <section>
+              <img src={`./${action.ha_action.ha_action_image}`} />
+              <hr />
+              <p>{action.ha_action.ha_action_text}</p>
+            </section>
+          );
+        })}
       </div>
       <div className="base-links">
-        {text.callToAction.map((action) => {
+        {text.home_links.map((link) => {
+          console.log(link);
           return (
             <div className="flex-inline">
-              <Picture s3ImgKey={action.pic.s3ImgKey} alt={action.pic.alt} />
-              <a href={action.url}>
-                <button>{action.text}</button>
+              <img src={`./${link.hl_link.hl_link_image}`} />
+              <a href={link.hl_link.hl_link_nav}>
+                <button>{link.hl_link.hl_link_text}</button>
               </a>
             </div>
           );
