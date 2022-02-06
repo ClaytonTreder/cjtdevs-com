9de3897ffc9a8308fcf0fba1010f7d58c2abadd5
Clayton Treder <cjtreder@gmail.com>
2022-02-06 14:53:47 -0500

    contact page done

--- src/screens/contact/Contact.jsx
+++ src/screens/contact/Contact.jsx
@@ -1,37 +1,20 @@
 import ContactForm from "components/Contact/Contact";
 import "./Contact.css";
 import Picture from "components/Picture/Picture";
-import { useEffect, useState } from "react";
+import { attributes } from "../../content/pages/contact.md";
 export default function Contact() {
-  const [text, setText] = useState({});
-  useEffect(() => {
-    text ??
-      fetch("/api/text/Contact")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          setText(() => data.content);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-  });
-  return text ? (
+  const text = attributes;
+  return (
     <div className="contact">
-      <Picture
-        s3ImgKey={text.pic.s3ImgKey}
-        alt={text.pic.alt}
-        className="bg-img"
-      />
+      <Picture src={text.background} alt="background" className="bg-img" />
       <title>
         <h2>{text.title}</h2>
       </title>
       <div className="subtitle">
-        <span style={{ fontSize: "large" }}>​​{text.subtitle}</span>
+        <span style={{ fontSize: "large" }}>​​{text.subTitle}</span>
       </div>
       <div className="subtitle">
-        <span style={{ fontSize: "medium" }}>{text.infoText}</span>
+        <span style={{ fontSize: "medium" }}>{text.content}</span>
       </div>
       <hr />
       <section>
@@ -40,5 +23,5 @@ export default function Contact() {
         </div>
       </section>
     </div>
-  ) : null;
+  );
 }
