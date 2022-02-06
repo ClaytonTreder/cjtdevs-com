4183e207611bd4b756fa21a29183714ab018470e
Clayton Treder <cjtreder@gmail.com>
2022-02-06 09:13:50 -0500

    services doe

--- src/screens/services/Services.jsx
+++ src/screens/services/Services.jsx
@@ -1,68 +1,57 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 import { useLocation } from "react-router";
 
 import "./Services.css";
 
+import { attributes } from "../../content/pages/services.md";
+
 import Contact from "components/Contact/Contact";
 import Picture from "components/Picture/Picture";
 import scrollTo from "shared/functions/scrollTo.js";
+import { useMemo } from "react";
 
 export default function Services() {
-  const [text, setText] = useState({});
+  const text = attributes;
 
   const talk = useRef(null);
 
   const loc = useLocation();
 
-  const scrollOptions = {
-    behavior: "auto",
-    block: "center",
-    inline: "center",
-  };
+  const scrollOptions = useMemo(
+    () => ({
+      behavior: "auto",
+      block: "center",
+      inline: "nearest",
+    }),
+    []
+  );
 
   useEffect(() => {
     switch (loc.hash) {
       case "#site":
-        text && scrollTo(text?.services[0]?.p_id, scrollOptions);
+        scrollTo(text?.sections[0]?.tag, scrollOptions);
         break;
       case "#app":
-        text && scrollTo(text?.services[1]?.p_id, scrollOptions);
+        scrollTo(text?.sections[1]?.tag, scrollOptions);
         break;
       case "#software":
-        text && scrollTo(text?.services[2]?.p_id, scrollOptions);
+        scrollTo(text?.sections[2]?.tag, scrollOptions);
         break;
       default:
         break;
     }
-  });
-
-  useEffect(() => {
-    text ??
-      fetch("/api/text/Services")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          setText(() => data.content);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-  });
+  }, [loc.hash, scrollOptions, text?.sections]);
 
   return text ? (
     <div className="services">
       <title>
         <h2>{text.title}</h2>
       </title>
-      <Picture
-        s3ImgKey={text.pic.s3ImgKey}
-        alt={text.pic.alt}
-        className="bg-img"
-      />
-      {text.services?.map((service, i) => {
+      <Picture src={text.background} alt="background pic" className="bg-img" />
+      {text.sections?.map((section, i) => {
         return (
           <div
+            id={section.tag}
             style={{
               backgroundColor: "rgba(255, 255, 255, 0.932)",
               margin: "10%",
@@ -71,15 +60,15 @@ export default function Services() {
             }}
             key={i}
           >
-            <h4>{service.title}</h4>
+            <h4>{section.title}</h4>
             <section>
-              <p id={service.p_id}>
+              <p>
                 <Picture
-                  s3ImgKey={service.s3ImgKey}
-                  alt={service.imgAlt}
-                  style={{ float: service.imgFloat }}
+                  src={section.pic}
+                  alt="service"
+                  style={{ float: section.float }}
                 />
-                {service.p_text}
+                {section.text}
               </p>
             </section>
             <div className="btn-container">
@@ -88,7 +77,7 @@ export default function Services() {
                   talk.current.scrollIntoView(scrollOptions);
                 }}
               >
-                {service.buttonText}
+                {section.button}
               </button>
             </div>
             <hr />
@@ -96,7 +85,7 @@ export default function Services() {
         );
       })}
       <div className="contact-container" ref={talk}>
-        <h4 className="lets-talk-h">{text.contact_title}</h4>
+        <h4 className="lets-talk-h">{text.contactTitle}</h4>
         <Contact />
       </div>
     </div>
