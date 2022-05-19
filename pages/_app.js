be0d5b9c07afa7a904d82d4b56cdb234a210a9af
Clayton Treder <cjtreder@gmail.com>
2022-05-19 09:43:27 -0400

    test

--- pages/_app.js
+++ pages/_app.js
@@ -6,30 +6,6 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <meta
-                    property="og:url"
-                    content="https://zesty-selkie-1384ae.netlify.app"
-                />
-                <meta
-                    key="ogtitle"
-                    property="og:title"
-                    content="CJT Devs - Find your home page here"
-                />
-                <meta
-                    key="ogdescription"
-                    property="og:description"
-                    content="CJT Devs is a colletive of software developers looking to create your next website or mobile application."
-                />
-                <meta
-                    key="ogimage"
-                    property="og:image"
-                    content="/images/preview.png"
-                />
-                <meta
-                    key="description"
-                    name="description"
-                    content="CJT Devs is a colletive of software developers looking to create your next website or mobile application."
-                />
             </Head>
             <Layout>
                 <Component {...pageProps} />
