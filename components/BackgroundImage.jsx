dd5bf1f9383c37fbed48887a04fc8baa18923b99
Clayton Treder <cjtreder@gmail.com>
2022-05-27 09:42:35 -0400

    update

--- components/BackgroundImage.jsx
+++ components/BackgroundImage.jsx
@@ -4,10 +4,7 @@ export default function BackgroundImage(props) {
     return props.src ? (
         <div className="bg-img">
             <Image
-                quality={25}
                 src={`/${props.src}`}
-                width={0}
-                height={0}
                 layout="fill"
                 alt={props.imgAlt}
             />
