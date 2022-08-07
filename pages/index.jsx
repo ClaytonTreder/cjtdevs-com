e3d6e4d915832d794616a57e43592ca9a775f69e
Clayton Treder <cjtreder@gmail.com>
2022-08-07 07:11:43 -0400

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,97 +1,113 @@
+import { useState } from 'react'
 import styles from '../styles/pages/Home.module.css'
 import { attributes } from '!!frontmatter-markdown-loader!../content/pages/home.md'
 import NewsLetter from '../components/NewsLetter'
 import Meta from './meta'
 import Image from 'next/image'
 import Link from 'next/link'
+import BackgroundImage from '../components/BackgroundImage'
+import Loader from '../components/Loader'
 
 function Home() {
     const text = attributes
+    const [state, setState] = useState({ loaded: false })
 
     return (
         <>
             <Meta />
             <div>
-                <div
-                    style={{
-                        backgroundImage: `url(/${text.home_bg})`,
+                <BackgroundImage
+                    src={text.home_bg}
+                    onload={() => {
+                        setState((prevState) => ({
+                            ...prevState,
+                            loaded: true,
+                        }))
                     }}
-                    className="bg-img"
-                ></div>
-                <div className={`${styles.banner} fade-in`}>
-                    <div className={`${styles.title}`}>
-                        {text.home_call_to_action.hcta_lines.map((line, i) => {
-                            return <h3 key={i}>{line.hcta_line}</h3>
-                        })}
-                    </div>
-                    <div className={styles.ribbon}>
-                        {text.home_call_to_action.hcta_buttons.map(
-                            (button, i) => {
-                                return (
-                                    <a
-                                        key={i}
-                                        href={
-                                            button.hcta_button.hcta_button_link
-                                        }
-                                    >
-                                        <button>
-                                            {
-                                                button.hcta_button
-                                                    .hcta_button_text
-                                            }
-                                        </button>
-                                    </a>
-                                )
-                            }
-                        )}
-                    </div>
-                </div>
-                <NewsLetter />
-                <div className={styles.information}>
-                    {text.home_actions.map((action, i) => {
-                        return (
-                            <section key={i}>
-                                <div
-                                    style={{
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        height: '5rem',
-                                    }}
-                                >
-                                    <Image
-                                        className={styles.img}
-                                        height={100}
-                                        width={100}
-                                        objectFit="contain"
-                                        alt="background"
-                                        src={`/${action.ha_action.ha_action_image}`}
-                                    />
-                                </div>
-                                <hr />
-                                <p>{action.ha_action.ha_action_text}</p>
-                            </section>
-                        )
-                    })}
-                </div>
-                <div className={styles.baseLinks}>
-                    {text.home_links.map((homeLink, i) => {
-                        return (
-                            <div key={i} className="flex-inline">
-                                <Image
-                                    height={80}
-                                    width={80}
-                                    src={`/${homeLink.hl_link.hl_link_image}`}
-                                    alt={`${homeLink.hl_link.hl_link_image}`}
-                                />
-                                <Link href={homeLink.hl_link.hl_link_nav}>
-                                    <button>
-                                        {homeLink.hl_link.hl_link_text}
-                                    </button>
-                                </Link>
+                />
+                {state.loaded ? (
+                    <>
+                        <div className={`${styles.banner} fade-in`}>
+                            <div className={`${styles.title}`}>
+                                {text.home_call_to_action.hcta_lines.map(
+                                    (line, i) => {
+                                        return <h3 key={i}>{line.hcta_line}</h3>
+                                    }
+                                )}
                             </div>
-                        )
-                    })}
-                </div>
+                            <div className={styles.ribbon}>
+                                {text.home_call_to_action.hcta_buttons.map(
+                                    (button, i) => {
+                                        return (
+                                            <a
+                                                key={i}
+                                                href={
+                                                    button.hcta_button
+                                                        .hcta_button_link
+                                                }
+                                            >
+                                                <button>
+                                                    {
+                                                        button.hcta_button
+                                                            .hcta_button_text
+                                                    }
+                                                </button>
+                                            </a>
+                                        )
+                                    }
+                                )}
+                            </div>
+                        </div>
+                        <NewsLetter />
+                        <div className={styles.information}>
+                            {text.home_actions.map((action, i) => {
+                                return (
+                                    <section key={i}>
+                                        <div
+                                            style={{
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                height: '5rem',
+                                            }}
+                                        >
+                                            <Image
+                                                className={styles.img}
+                                                height={100}
+                                                width={100}
+                                                objectFit="contain"
+                                                alt="background"
+                                                src={`/${action.ha_action.ha_action_image}`}
+                                            />
+                                        </div>
+                                        <hr />
+                                        <p>{action.ha_action.ha_action_text}</p>
+                                    </section>
+                                )
+                            })}
+                        </div>
+                        <div className={styles.baseLinks}>
+                            {text.home_links.map((homeLink, i) => {
+                                return (
+                                    <div key={i} className="flex-inline">
+                                        <Image
+                                            height={80}
+                                            width={80}
+                                            src={`/${homeLink.hl_link.hl_link_image}`}
+                                            alt={`${homeLink.hl_link.hl_link_image}`}
+                                        />
+                                        <Link
+                                            href={homeLink.hl_link.hl_link_nav}
+                                        >
+                                            <button>
+                                                {homeLink.hl_link.hl_link_text}
+                                            </button>
+                                        </Link>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </>
+                ): <Loader />}
             </div>
         </>
     )
