a62b56c2b43c4b0447aa020888940072bf30b4df
claytontreder <cjtreder@hotmail.com>
2023-01-25 08:12:07 -0500

    remove gtag

--- pages/_document.js
+++ pages/_document.js
@@ -10,32 +10,6 @@ class MyDocument extends Document {
                     />
                     <meta name="robots" content="index, follow" />
                     <link rel="shortcut icon" href="/favicon.ico" />
-                    <script
-                        async
-                        src="https://www.googletagmanager.com/gtag/js?id=UA-198230094-1"
-                    />
-
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                          
-                            gtag('config', 'UA-198230094-1');`,
-                        }}
-                    />
-                    <script
-                        async
-                        src="https://www.googletagmanager.com/gtag/js?id=G-41KMBS1TNV"
-                    ></script>
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `window.dataLayer = window.dataLayer || [];
-                                   function gtag(){dataLayer.push(arguments);}
-                                   gtag('js', new Date());
-                                   gtag('config', 'G-41KMBS1TNV');`,
-                        }}
-                    />
                 </Head>
                 <body>
                     <Main />
