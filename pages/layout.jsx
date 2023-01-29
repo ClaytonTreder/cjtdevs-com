a75e5ac8994a9a1bece75f20948fd2a1cc1f49aa
claytontreder <cjtreder@hotmail.com>
2023-01-29 11:53:57 -0500

    test

--- pages/layout.jsx
+++ pages/layout.jsx
@@ -3,9 +3,34 @@ import { useState } from 'react'
 import Header from '../components/Header'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+import { useEffect } from 'react'
 
 export default function Layout({ children }) {
     const [mobileNavOpen, setMobileNavOpen] = useState()
+
+    useEffect(() => {
+        if (typeof window !== undefined) {
+            var chatbox = document.getElementById('fb-customer-chat')
+            chatbox.setAttribute('page_id', '287773108038402')
+            chatbox.setAttribute('attribution', 'biz_inbox')
+            window.fbAsyncInit = function () {
+                FB.init({
+                    xfbml: true,
+                    version: 'v15.0',
+                })
+            }
+            ;(function (d, s, id) {
+                var js,
+                    fjs = d.getElementsByTagName(s)[0]
+                if (d.getElementById(id)) return
+                js = d.createElement(s)
+                js.id = id
+                js.src =
+                    'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
+                fjs.parentNode.insertBefore(js, fjs)
+            })(document, 'script', 'facebook-jssdk')
+        }
+    }, [])
 
     return (
         <>
@@ -18,6 +43,8 @@ export default function Layout({ children }) {
                 mobileNavOpen={mobileNavOpen}
             />
             {children}
+            <div id="fb-root"></div>
+            <div id="fb-customer-chat" className="fb-customerchat"></div>
             <Footer />
         </>
     )
